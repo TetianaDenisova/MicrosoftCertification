a782233a3773732ebd85a589cefb71c56e44124c
Denisova <tania.shtrix15@gmail.com>
2018-06-17 11:37:57 +0300

    init

---
+++ Part2/Part2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
